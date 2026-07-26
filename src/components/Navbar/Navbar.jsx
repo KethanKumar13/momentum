5bc0b0890b59a28eed3f734c20f306b28093c846
Kethan Kumar <kethankumar136@gmail.com>
2026-07-26 18:47:00 +0530

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav style={{ padding: '1rem 2rem', background: '#1f2937', color: '#fff' }}>
+      <h2 style={{ margin: 0 }}>TaskFlow</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
