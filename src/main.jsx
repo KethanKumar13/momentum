ee7776ac36483bf4c0f6d6b1fc73644616038b0e
Kethan Kumar <kethankumar136@gmail.com>
2026-08-01 13:15:19 +0530

    style: introduce global design system

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./styles/variables.css";
+import "./styles/global.css";
+import "./styles/typography.css";
+import "./styles/animations.css";
+import "./styles/utilities.css";
 import './index.css'
 import App from './App.jsx'
 
